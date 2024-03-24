ec60247bad0f1d63919005072dc3e3782e5de06f
lsc713 <lsc7134.naver.com>
2024-03-24 22:40:26 +0900

    test commit

---
+++ src/main/java/sample/cafekiosk/unit/beverages/Americano.java
@@ -0,0 +1,14 @@
+package sample.cafekiosk.unit.beverages;
+
+public class Americano implements Beverage {
+
+    @Override
+    public String getName() {
+        return "Americano";
+    }
+
+    @Override
+    public int getPrice() {
+        return 4000;
+    }
+}
