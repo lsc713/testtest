cfcf77b559c17f52a14788e9f6c684e87ac2fe53
lsc713 <lsc7134.naver.com>
2024-04-26 16:33:11 +0900

    tdd practice without @Mock annotation

---
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,42 @@
+package sample.cafekiosk.spring.api.service.mail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+class MailServiceTest {
+
+    @Test
+    @DisplayName("메일 전송 테스트")
+    public void sendMail() throws Exception{
+        //given
+        MailSendClient mailSendClient = mock(MailSendClient.class);
+        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
+
+        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        when(mailSendClient.sendEmail(anyString(), any(String.class), any(String.class), any(String.class)))
+            .thenReturn(true);
+
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+
+    }
+
+}
