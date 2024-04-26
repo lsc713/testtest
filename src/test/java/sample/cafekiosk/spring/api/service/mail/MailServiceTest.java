b94ca33a7b8b727b9e871f1ee81fa2eb7008ce9b
lsc713 <lsc7134.naver.com>
2024-04-26 16:41:31 +0900

    tdd practice without @Mock + @InjectMocks annotation

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -10,23 +10,30 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
+    @Mock
+    private MailSendClient mailSendClient;
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    //해당 생성자를 보고 mock으로 선언 인자 주입 = 'inject'
+    private MailService mailService;
     @Test
     @DisplayName("메일 전송 테스트")
     public void sendMail() throws Exception{
         //given
-        MailSendClient mailSendClient = mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
-
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         when(mailSendClient.sendEmail(anyString(), any(String.class), any(String.class), any(String.class)))
             .thenReturn(true);
 
